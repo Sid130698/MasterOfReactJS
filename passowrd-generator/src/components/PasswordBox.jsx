91b85426c9ef7dcc1f224a51daea59a1c5ae50fa
Sid130698 <sidsingh130698@gmail.com>
2024-05-11 23:57:31 +0530

    password generator works wonderfully.

--- passowrd-generator/src/components/PasswordBox.jsx
+++ passowrd-generator/src/components/PasswordBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import ToolBar from './ToolBar';
 
 const PasswordBox = () => {
@@ -7,7 +7,8 @@ const PasswordBox = () => {
     const [isNumAllowed, setNumAllowed] = useState(false);
     const [generatedpassword, setGeneratedPassword] = useState("password");
     const [copyBtn, setCopyButton] = useState("copy");
-
+    //useRef hook
+    const passwordRef = useRef()
     const passwordGenerator = useCallback(() => {
         let password = "";
         let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -18,19 +19,30 @@ const PasswordBox = () => {
             password += str.charAt(charIndex);
         }
         setGeneratedPassword(password);
+        setCopyButton("Copy")
     }, [length, isNumAllowed, isSpCharAllowed,setGeneratedPassword]);
 
     useEffect(() => {
         passwordGenerator();
     }, [length, isNumAllowed, isSpCharAllowed, passwordGenerator]);
-
+    const copyPasswordToClipBoard = useCallback(() => {
+        passwordRef.current?.select();
+        window.navigator.clipboard.writeText(generatedpassword);
+        setCopyButton("Copied✔️");
+    },[generatedpassword]);
     return (
         <div className='w-full flex justify-center'>
             <div className='w-full max-w-xl bg-cyan-900 px-5 py-6'>
                 <div className='text-center mb-2 text-xl text-white'>Password generator </div>
                 <div className='flex items-center justify-center'>
-                    <div className='box-border bg-white px-3 py-3 rounded-l-lg w-80'>{generatedpassword}</div>
-                    <button className='outline-none bg-blue-400 py-3 px-3 rounded-r-lg w-20'>{copyBtn}</button>
+                    <input 
+                        type="text" 
+                        className='box-border bg-white px-3 py-3 rounded-l-lg w-80' 
+                        ref={passwordRef} 
+                        readOnly 
+                        value={generatedpassword}
+                    />
+                    <button className='outline-none bg-blue-400 py-3 px-3 pr-20 rounded-r-lg w-20' onClick={copyPasswordToClipBoard}>{copyBtn}</button>
                 </div>
                 <ToolBar length={length} setLength={setLength} num={isNumAllowed} setNum={setNumAllowed} spChar={isSpCharAllowed} setSPChar={setSPCharAllowed} />
             </div>
